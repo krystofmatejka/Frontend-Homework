e378d3ab78ef8064747a8c6ed526894f79656d27
Krystof Matejka <krystof.matejka@pm.me>
2025-11-14 13:00:59 +0100

    final cleanup

--- app/detail/[id]/error.tsx
+++ app/detail/[id]/error.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 export default function DetailError({ error, reset }: { error: Error; reset: () => void }) {
-    return (
-        <div>
-            <h1>Something went wrong.</h1>
-            <p>{error.message}</p>
-            <button onClick={() => reset()} className="primary-button">Try again</button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Something went wrong.</h1>
+      <p>{error.message}</p>
+      <button onClick={() => reset()} className="primary-button">Try again</button>
+    </div>
+  )
 }
